6cf16432556eb1d469afb4d0d183657ae9807b73
alibombaye <alibombaye@gmail.com>
2020-01-16 12:08:55 +0000

    messing about with streaming

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import axios from 'axios';
 import { getMarketingPageDashboardContext, postSubmitForModeration } from './pages/dashboard/controller';
 import { getSubDashboardPageContext } from './pages/dashboard/subDashboards/controller';
 import { getSectionPageContext, getSectionPageErrorContext, postSection } from './pages/section/controller';
 import { getPreviewPageContext } from './pages/preview/controller';
 import logger from './logger';
 import { errorHandler } from './pages/error/errorHandler';
+import fs from 'fs';
+// const http = require('http');
 
 const router = express.Router();
 
@@ -12,6 +15,25 @@ router.get('/healthcheck', async (req, res) => {
   logger.info('navigating to healthcheck page');
 
   res.send('Marketing pages is Running!!!');
+});
+
+
+router.get('/download', async (req, res) => {
+  logger.info('Gonna try a download');
+
+  // const file = fs.createReadStream('./test-file.png');
+  // console.log(`file is ${JSON.stringify(file)}`);
+  const code = '';
+
+  const endpoint = `https://gpit-func-app.azurewebsites.net/api/DownloadFileSdkTest/docs/NHSmail.pdf?code=${code}`;
+  logger.info(`api called: [GET] ${endpoint}`);
+  const response = await axios.get(endpoint, { responseType: 'stream' });
+  // console.log(`the file ${JSON.stringify(theFile)}`);
+
+  response.data.pipe(res);
+  // res.pipe(file);
+
+  // res.send('Marketing pages is Running!!!');
 });
 
 router.get('/solution/:solutionId', async (req, res, next) => {
